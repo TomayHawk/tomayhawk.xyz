3b231da182122a2b02dda9781bce6c8b351b9bea
TomayHawk <gmtomayhawk@gmail.com>
2025-01-03 23:58:31 -0800

    Minor Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ function Home() {
         <>
             <div className="main-container">
                 <div className="main-name-container">
+                    <div className="main-picture"></div>
                     <div className="main-name-text">Toma Yuen</div>
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
